bca473ef0418f778d53b15c316cc5c137bba6ba1
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2024-09-10 19:44:05 -0400

    Added some Assets

---
+++ Last Summer/Assets/Scripts/Radio_Volume.cs
@@ -0,0 +1,39 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio_Volume : MonoBehaviour
+{
+    public float radioInFrontDistance;
+
+    public LayerMask radioLayerMask;
+    //private float facing = 1;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDistance();
+
+        if (radioInFrontDistance > 10)
+        {
+            this.GetComponent<AudioSource>().;
+        }
+    }
+
+// call every frame to determine the distance of the radio from the player
+private void UpdateDistance()
+    {
+        radioInFrontDistance = Vector2.Distance(this.transform.position, player.transform.position);
+
+        Debug.Log(radioInFrontDistance);
+    }
+}
