e5277038b356fcf6294b193bf1a03750d52d81fc
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2024-09-24 17:47:54 -0400

    Update Player_Movement.cs

--- Last Summer/Assets/Scripts/Player_Movement.cs
+++ Last Summer/Assets/Scripts/Player_Movement.cs
@@ -9,8 +9,8 @@ public class script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
     }
+    
 
     // Update is called once per frame
     void Update()
